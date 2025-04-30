e4814e38be013635e1d42e67d5f1d4806a723fe4
Curikh <vladkrutikov1980@gmail.com>
2025-04-30 22:05:13 +0300

    beta 0.2
    Slot select added
    hp and coin started

--- Assets/InventoryManager.cs
+++ Assets/InventoryManager.cs
@@ -1,59 +1,60 @@
 using UnityEngine;
-public class InventoryManager: MonoBehaviour
+
+public class InventoryManager : MonoBehaviour
 {
-public int maxStackedItems = 10;
-public InventorySlot[] inventorySlots;
-public GameObject inventoryItemPrefab;
+    public int maxStackedItems = 10;
+    public InventorySlot[] inventorySlots;
+    public GameObject inventoryItemPrefab;
+    int selectedSlot = -1;
 
-/*
-int selectedSlot = -1;
-
-private void Start() {
-    ChangeSelectedSlot(0);
-}
-void ChangeSelectedSlot(int newValue) {
-    if (selectedSlot >= 0) {
-            inventorySlots[selectedSlot].Deselect();
+    private void Start() {
+        ChangeSelectedSlot(0); // Выбираем первый слот по умолчанию
     }
-
-
-    inventorySlots[newValue].Select();
-    selectedSlot = newValue;
-}
-*/
-
-// проверка на смежные предметы
-public bool AddItem(Item item) 
-{
-    for (int i = 0; i < inventorySlots.Length; i++) 
-    { 
-    InventorySlot slot = inventorySlots[i];
-    InventoryItem itemInSlot = slot.GetComponentInChildren<InventoryItem>(); 
-        if (itemInSlot != null && itemInSlot.item == item && itemInSlot.count < maxStackedItems && itemInSlot.item.stackable == true) 
-        {
-        itemInSlot.count++;
-        itemInSlot.RefreshCount();
-        return true;
+    private void Update(){
+        if (Input.inputString != null) {
+            bool isNumber = int.TryParse(Input.inputString, out int number);
+            if (isNumber && number > 0 && number < 5) {
+                ChangeSelectedSlot(number-1);
+            }
         }
     }
-
-// проверка на пустые слоты
-
-    for (int i = 0; i < inventorySlots.Length; i++) 
-    { 
-    InventorySlot slot = inventorySlots[i];
-    InventoryItem itemInSlot = slot.GetComponentInChildren<InventoryItem>(); 
-        if (itemInSlot == null) 
-        {
-        SpawnNewItem(item, slot);
-        return true;
+    void ChangeSelectedSlot(int newValue) {
+        if (selectedSlot >= 0) {
+            inventorySlots[selectedSlot].Deselect(); // Снимаем выделение
         }
+        
+        inventorySlots[newValue].Select(); // Выделяем новый слот
+        selectedSlot = newValue;
     }
-    return false;
-}
-void SpawnNewItem(Item item, InventorySlot slot) {
-    GameObject newItemGo = Instantiate(inventoryItemPrefab, slot.transform);
-    InventoryItem inventoryItem = newItemGo.GetComponent<InventoryItem>();
-    inventoryItem.InitialiseItem(item);
+
+    public bool AddItem(Item item) {
+        // Проверка на стекирование
+        for (int i = 0; i < inventorySlots.Length; i++) {
+            InventorySlot slot = inventorySlots[i];
+            InventoryItem itemInSlot = slot.GetComponentInChildren<InventoryItem>();
+            if (itemInSlot != null && itemInSlot.item == item && 
+                itemInSlot.count < maxStackedItems && itemInSlot.item.stackable) {
+                itemInSlot.count++;
+                itemInSlot.RefreshCount();
+                return true;
+            }
+        }
+        
+        // Проверка на пустые слоты
+        for (int i = 0; i < inventorySlots.Length; i++) {
+            InventorySlot slot = inventorySlots[i];
+            InventoryItem itemInSlot = slot.GetComponentInChildren<InventoryItem>();
+            if (itemInSlot == null) {
+                SpawnNewItem(item, slot);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void SpawnNewItem(Item item, InventorySlot slot) {
+        GameObject newItemGo = Instantiate(inventoryItemPrefab, slot.transform);
+        InventoryItem inventoryItem = newItemGo.GetComponent<InventoryItem>();
+        inventoryItem.InitialiseItem(item);
     }
 }
--- Assets/InventorySlot.cs
+++ Assets/InventorySlot.cs
@@ -4,20 +4,18 @@ using UnityEngine.UI;
 
 public class InventorySlot : MonoBehaviour, IDropHandler
 {
- /*   public Image image;
-    public Color selectedColor, notSelectedColor;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-   
-   public void Awake() {
-        Deselect();
-   }
-   public void Select() {
-        image.color = selectedColor;
-   }
-   public void Deselect(){
-        image.color = notSelectedColor;
-   }
-   */
+    public Image slotImage; // Image компонент слота
+    public Sprite defaultSlotSprite; // Обычный спрайт слота
+    public Sprite selectedSlotSprite; // Спрайт для выбранного слота
+    
+    public void Select() {
+        slotImage.sprite = selectedSlotSprite; // Меняем спрайт на "выбранный"
+    }
+    
+    public void Deselect() {
+        slotImage.sprite = defaultSlotSprite; // Возвращаем обычный спрайт
+    }
+
     public void OnDrop(PointerEventData eventData) {
         if (transform.childCount == 0) {
             InventoryItem inventoryItem = eventData.pointerDrag.GetComponent<InventoryItem>();
