2b23beaafa1512529eca571b4e2d309acc13d737
lunaro-4 <pavelike@gmail.com>
2025-05-03 18:52:28 +0300

    fix(SpawnItem): add reference to INventory namespace; add correct intendations

--- Assets/DungeonGenerator/Scripts/SpawnItem.cs
+++ Assets/DungeonGenerator/Scripts/SpawnItem.cs
@@ -1,11 +1,12 @@
-       using UnityEngine;
-       
-       public class DemoScript : MonoBehaviour
-       {
-        public InventoryManager inventoryManager;
-        public Item[] itemsToPickup;
-        public void PickUpItem (int id) {
-        inventoryManager.AddItem(itemsToPickup[id]);
-        }
-       }
+using UnityEngine;
+using Inventory;
+
+public class DemoScript : MonoBehaviour
+{
+	public InventoryManager inventoryManager;
+	public Item[] itemsToPickup;
+	public void PickUpItem (int id) {
+		inventoryManager.AddItem(itemsToPickup[id]);
+	}
+}
 
